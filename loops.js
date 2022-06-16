f38354219d7fe9b0cbb6c5bd91139a4c23634716
bder00s <bonne-deroos@hotmail.com>
2022-06-16 17:13:39 +0200

    alleen laatste deel van de bonus opdracht nog doen

--- loops.js
+++ loops.js
@@ -5,7 +5,10 @@
 // Hoera!
 // Hoera!
 // ==========================================
+for (let i = 0; i < 3; i++) {
+    console.log("Hoera");
 
+}
 
 
 // ==========================================
@@ -17,8 +20,11 @@
 // loop...
 // klaar!
  // ==========================================
+for (let i = 0; i < 4; i++) {
+    console.log("loop");
 
-
+}
+console.log("klaar");
 
 // ==========================================
 // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
@@ -29,6 +35,17 @@
 // 3 is oneven
 // .... etc.
 // 15 is oneven
+
+for (let i = 0; i < 15; i++) {
+   if (i % 2 === 0) {
+       console.log(i + " is even");
+   }
+ else {
+     console.log(i + " is oneven");
+   }
+}
+
+
 
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over. Google daarom eens naar 'mdn webdocs remainder operator'.
 // ==========================================
@@ -50,4 +67,23 @@
 // >> 8
 // >> 9
 // ==========================================
+
+for (let i = 0; i < 10; i++)
+    if (i >= 3) {
+        console.log("> " + i);
+}
+else if (  (i <= 3) ){
+    console.log(i);
+}
+
+else ( (i <= 6) ) {
+    console.log(">> " + i);
+
+}
+
+
+
+
+
+    // console.log(i);
 
